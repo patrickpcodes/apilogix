a87d95c442583e4ea4b569be37b2ad7ad840f780
Patrick Petropoulos <patrickpetropoulos@gmail.com>
2024-09-15 17:06:12 -0400

    Test github action

---
+++ apilogix-react-app/__tests__/example.test.tsx
@@ -0,0 +1,13 @@
+import { render, screen } from "@testing-library/react";
+import Home from "../app/page";
+import "@testing-library/jest-dom";
+
+describe("Home", () => {
+  it("renders a heading", () => {
+    render(<Home />);
+    const heading = screen.getByRole("heading", {
+      name: /Welcome to My Homepage/i,
+    });
+    expect(heading).toBeInTheDocument(); // Updated to match the new heading
+  });
+});
